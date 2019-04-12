837348523b483d6710101d6e38c56da16384ea2b
Kaleb Sverdrup <kalebfire@cox.net>
2019-04-12 16:46:50 -0700

    Test

---
+++ RobotCode/inc/marcros.h
@@ -0,0 +1,26 @@
+
+#ifndef MACRO
+#define MACRO
+
+#define FL_motorEN 0
+#define FL_motorForward 0
+#define FL_motorReverse 0
+#define FL {FL_motorEN,FL_motorForward,FL_motorReverse}
+
+#define FR_motorEN 0
+#define FR_motorForward 0
+#define FR_motorReverse 0
+#define FR {FR_motorEN,FR_motorForward,FR_motorReverse}
+
+#define BL_motorEN 0
+#define BL_motorForward 0
+#define BL_motorReverse 0
+#define BL {BL_motorEN,BL_motorForward,BL_motorReverse}
+
+#define BR_motorEN 0
+#define BR_motorForward 0
+#define BR_motorReverse 0
+#define BR {BR_motorEN,BR_motorForward,BR_motorReverse}
+
+
+#endif
