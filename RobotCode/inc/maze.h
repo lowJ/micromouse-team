58c70e362444af6e09dee74d394282cfff89738b
Kaleb Sverdrup <kalebfire@cox.net>
2019-04-22 17:21:14 -0700

    Finished Maze class

--- RobotCode/inc/maze.h
+++ RobotCode/inc/maze.h
@@ -3,6 +3,26 @@
 #ifndef MAZE
 #define MAZE
 
+struct block {
+    float time;
+    bool mark;
+    bool * moves;
+    int * previous;
+    block();
+};
 
+class Maze
+{
+    block maze[16][16];
+    int * start;   
+    int (*goal)[2];
+
+public:
+    Maze();
+    float getTime(int x, int y);
+    bool isMarked(int x, int y);
+    int * getPrevious(int x, int y);
+    bool hasWall(int x, int y, int direction);
+};
 
 #endif
--- RobotCode/src/maze.cpp
+++ RobotCode/src/maze.cpp
@@ -1,3 +1,44 @@
 // defines the maze and functions to easily acess data within it
 
 #include "../inc/maze.h"
+
+block::block() 
+{
+    this->mark = false;
+    this->time = 0;
+    this->moves = new bool[4] {false, false, false, false};
+    this->previous = new int[3] {-1,-1,-1};
+};
+
+float Maze::getTime(int x, int y) 
+{
+    return maze[x][y].time;
+}
+
+bool Maze::isMarked(int x, int y)
+{
+    return maze[x][y].mark;
+}
+
+int * Maze::getPrevious(int x, int y)
+{
+    return maze[x][y].previous;
+}
+
+bool Maze::hasWall(int x, int y,int direction)
+{
+    return maze[x][y].moves[direction];
+}
+
+Maze::Maze() 
+{
+    for (int i = 0; i < 16; i++) 
+    {
+        for (int j = 0; j < 16; j++) 
+        {
+            this->maze[i][j] = block();
+        }
+    }
+    this->start = new int[3] {15,0,0};
+    goal = new int[4][2] {{7,7},{7,8},{8,7},{8,8}};
+}
